18f681a0c158b86b7a9a390233e7c1d185fe04e0
Will Sneddon <will@sneddon.dev>
2023-12-06 16:44:21 -0500

    Copy input file to output di, implement part 2

--- 01/Program.cs
+++ 01/Program.cs
@@ -3,22 +3,67 @@ using System.Text.RegularExpressions;
 
 Console.WriteLine("Day 1");
 long part1Resultant = 0;
+long part2Resultant = 0;
 
 foreach (var line in File.ReadLines(@"calibrationvalues.txt"))
 {
-    var first = ForwardMatch().Match(line);
-    var last = ReverseMatch().Match(line);
-
-    part1Resultant += int.Parse(string.Concat(first.Value, last.Value)); 
+    part1Resultant += GetLinePart1Value(line);
+    part2Resultant += GetPart2LineValue(line);
 }
 
-Console.WriteLine($"Resultant is: {part1Resultant}");
+Console.WriteLine($"PART 1 Resultant is: {part1Resultant}");
+Console.WriteLine($"PART 2 Resultant is: {part2Resultant}");
 
 partial class Program
 {
     [GeneratedRegex("[0-9]")]
-    private static partial Regex ForwardMatch();
+    private static partial Regex ForwardNumberMatch();
 
     [GeneratedRegex("[0-9]", RegexOptions.RightToLeft)]
-    private static partial Regex ReverseMatch();
+    private static partial Regex ReverseNumberMatch();
+
+    [GeneratedRegex("[0-9]|one|two|three|four|five|six|seven|eight|nine")]
+    private static partial Regex ForwardTextAndNumberMatch();
+
+    [GeneratedRegex("[0-9]|one|two|three|four|five|six|seven|eight|nine", RegexOptions.RightToLeft)]
+    private static partial Regex ReverseTextAndNumberMatch();
+
+    private static readonly Dictionary<string,string> numberStrings = new()
+    {
+        { "one", "1" },
+        { "two", "2" },
+        { "three", "3" },
+        { "four", "4" },
+        { "five", "5" },
+        { "six", "6" },
+        { "seven", "7" },
+        { "eight", "8" },
+        { "nine", "9" }
+    };
+
+    private static int GetLinePart1Value(string line)
+    {
+        var first = ForwardNumberMatch().Match(line).Value;
+        var last = ReverseNumberMatch().Match(line).Value;
+
+        return int.Parse(string.Concat(first, last)); 
+    }
+
+    private static int GetPart2LineValue(string line)
+    {
+        var first = ForwardTextAndNumberMatch().Match(line).Value;
+        var last = ReverseTextAndNumberMatch().Match(line).Value;
+
+        if(numberStrings.ContainsKey(first))
+        {
+            first = numberStrings[first];
+        }
+
+        if(numberStrings.ContainsKey(last))
+        {
+            last = numberStrings[last];
+        }
+
+        return int.Parse(string.Concat(first, last)); 
+    }
 }
