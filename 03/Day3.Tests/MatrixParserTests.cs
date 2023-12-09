e089c242636c1f32d32c3f0b58b88e092cc62b47
Will Sneddon <will@sneddon.dev>
2023-12-09 02:42:42 -0500

    Simpler implementation is sometimes better

--- 03/Day3.Tests/MatrixParserTests.cs
+++ 03/Day3.Tests/MatrixParserTests.cs
@@ -2,45 +2,67 @@ namespace Day3.Tests;
 
 public class MatrixParserTest
 {
-    [Theory]
-    [InlineData("467..114..", 2, "7", "467")]
-    [InlineData("...*......", -1, null, null)]
-    [InlineData("..35..633.", 3, "5", "35")]
-    [InlineData("..35..633.", 6, "6", "633")]
-    [InlineData("617*......", 2, "7", "617")]
-    [InlineData("..592.....", 4, "2", "592")]
-    [InlineData("......755.", 6, "7", "755")]
-    [InlineData(".664.598..", 3, "4", "664")]
-    [InlineData(".664.598..", 5, "5", "598")]
-    public void ExtractAdjacentPartNumberShouldPass(string partNumberLine, int position, string initialMatch, string expectedResult)
-    {
-        var parser = new MatrixParser();
-        var partNumber = parser.ExtractAdjacentPartNumber(partNumberLine, position, initialMatch);
+    // [Theory]
+    // [InlineData("467..114..", 2, "7", "467")]
+    // [InlineData("...*......", -1, null, null)]
+    // [InlineData("..35..633.", 3, "5", "35")]
+    // [InlineData("..35..633.", 6, "6", "633")]
+    // [InlineData("617*......", 2, "7", "617")]
+    // [InlineData("..592.....", 4, "2", "592")]
+    // [InlineData("......755.", 6, "7", "755")]
+    // [InlineData(".664.598..", 3, "4", "664")]
+    // [InlineData(".664.598..", 5, "5", "598")]
+    // public void ExtractAdjacentPartNumberShouldPass(string partNumberLine, int position, string initialMatch, string expectedResult)
+    // {
+    //     var parser = new MatrixParser();
+    //     var partNumber = parser.ExtractAdjacentPartNumber(partNumberLine, position, initialMatch);
 
-        Assert.Equal(expectedResult, partNumber);
-    }
+    //     Assert.Equal(expectedResult, partNumber);
+    // }
 
     [Theory]
-    [InlineData(new string[] { "467..114..", "...*......", "..35..633." }, 3, new long[] { 467, 35 })]
-    [InlineData(new string[] { "..35..633.", "......#...", "617*......" }, 6, new long[] { 633 })]
-    [InlineData(new string[] { "......#...", "617*......", ".....+.58." }, 3, new long[] { 617 })]
-    [InlineData(new string[] { "...184", "..*...", "...571" }, 2, new long[] { 184, 571 })]
-    public void ExtractAllPartNumbersShouldPass(string[] partLines, int symbolMatchIndex, long[] expectedParts)
+    [InlineData(new string[] { "467..114..", "...*......", "..35..633." }, new int[] { 3 }, new long[] { 467, 35 })]
+    [InlineData(new string[] { "..35..633.", "......#...", "617*......" }, new int[] { 6 }, new long[] { 633 })]
+    [InlineData(new string[] { "......#...", "617*......", ".....+.58." }, new int[] { 3 }, new long[] { 617 })]
+    [InlineData(new string[] { "...184", "..*...", "...571" }, new int[] { 2 }, new long[] { 184, 571 })]
+    [InlineData(new string[] {
+    "..........*..../.......337..475*......&..391...347...795....*.........................*......722..666...............@.......................",
+    "........823.$.....+.../.........716......*........*....*.....247......329...........697......*.....*....%.....168....624.........592........",
+    "...98*.......916..915......245............277..&..353...719..........$.....................846.601.37...47......................*......519.."
+    }, new int[] { 12 }, new long[] { 916 })]
+    public void ExtractAllPartNumbersShouldPass(string[] partLines, int[] symbolIndices, long[] expectedParts)
     {
+        var searchRadius = 1;
         var parser = new MatrixParser();
-        var parts = parser.FindPartNumbers(partLines, symbolMatchIndex);
+        var parts = parser.ExtractIntersectingPartNumbers(partLines, symbolIndices, searchRadius);
 
         Assert.Equal(expectedParts.Length, parts.Count());
         Assert.Equal(expectedParts, parts.ToArray());
     }
 
     [Theory]
-    [InlineData( @"testinput.txt", 4361)]
+    [InlineData(@"testinput.txt", 4361)]
     public void ProcessEntireFileSumOfPartsMatchShouldPass(string fileName, long expectedSum)
     {
         var parser = new MatrixParser();
         var sumOfParts = parser.ParseInputAndSumParts(fileName);
 
         Assert.Equal(expectedSum, sumOfParts);
+    }
+
+    [Theory]
+    [InlineData("", 0)]
+    [InlineData("...$.*....", 2)]
+    [InlineData(".664.598..", 0)]
+    [InlineData(".....+.58.", 1)]
+    [InlineData(".........511..853......*..............*...........355.............35-.....-....@..636.....=...595..131...496..........................=687..", 7)]
+    [InlineData("..........*..../.......337..475*......&..391...347...795....*.........................*......722..666...............@.......................", 7)]
+    [InlineData("........823.$.....+.../.........716......*........*....*.....247......329...........697......*.....*....%.....168....624.........592........", 9)]
+    [InlineData("...98*.......916..915......245............277..&..353...719..........$.....................846.601.37...47......................*......519..", 4)]
+    public void NumSymbolMatchesPerLineShouldPass(string line, int expectedMatches)
+    {
+        var matches = MatrixParser.SymbolNoNumbersNoDots().Matches(line);
+
+        Assert.Equal(expectedMatches, matches.Count);
     }
 }
--- 03/Day3/MatrixParser.cs
+++ 03/Day3/MatrixParser.cs
@@ -1,103 +1,68 @@
 using System.Text.RegularExpressions;
 
 public partial class MatrixParser
-{    
+{
     public long ParseInputAndSumParts(string fileName)
     {
-        IEnumerable<long> PartNumbers = new List<long>();
+        var PartNumbers = new List<long>();
 
         int lineNo = 0;
+        var searchSize = 1;
         var symbolRegex = SymbolNoNumbersNoDots();
         var lineBuffer = File.ReadAllLines(fileName);
 
-        try
+        foreach (var line in lineBuffer)
         {
-            foreach (var line in lineBuffer)
+            var symbolIndices = symbolRegex.Matches(line).Select(m => m.Index);
+            if (!symbolIndices.Any())
             {
-                var symbolResults = symbolRegex.Match(line);
-                while(symbolResults.Success)
-                {
-                    //We have line and index of each symbol any number in 1 radius in all directions including diagonal must be captured as a whole
-                    //Adacent coords are same/+1/-1 line number, same/+1/-1 index
-                    //if we are at the first line or the last line we cannot read behind or ahead
-                    //If the symbol is the first or last column, we can't check ahead or behind
-                    var prevLine = lineNo == 0 ? string.Empty : lineBuffer[lineNo - 1];
-                    var nextLine = lineNo == lineBuffer.Length - 1 ? string.Empty : lineBuffer[lineNo + 1];
-
-                    var parts = FindPartNumbers(new string[] { prevLine, line, nextLine}, symbolResults.Index);
-                    PartNumbers = PartNumbers.Union(parts);
-
-                    symbolResults = symbolResults.NextMatch();
-                }
-
                 lineNo++;
+                continue;
             }
-        } 
-        catch(Exception ex)
-        {
-            Console.WriteLine($"Error parsing line {lineNo}");
-            throw ex;
+
+            var prevLine = lineNo == 0 ? string.Empty : lineBuffer[lineNo - searchSize];
+            var nextLine = lineNo + searchSize >= lineBuffer.Length ? string.Empty : lineBuffer[lineNo + searchSize];
+
+            var parts = ExtractIntersectingPartNumbers(new string[] { prevLine, line, nextLine }, symbolIndices, searchSize);
+            PartNumbers.AddRange(parts);
+
+            lineNo++;
         }
-     
+
         return PartNumbers.Sum();
     }
 
-    public IEnumerable<long> FindPartNumbers(string[] linesToSearch, int symbolIndex, int radius = 1)
+    public IEnumerable<long> ExtractIntersectingPartNumbers(string[] lines, IEnumerable<int> symbolIndices, int searchRadius)
     {
-        //TODO: use the radius other than as a magic number
-        var parts = new List<long>();
+        var partNumbers = new List<long>();
 
-        var startMatchIndex = symbolIndex == 0 ? symbolIndex : symbolIndex - radius;
-        var searchLength = radius * 3 + (symbolIndex == linesToSearch.First().Length - 1 ? - 1 : 0);
-        
-        foreach(var line in linesToSearch)
+        foreach (var line in lines)
         {
-            var match = NumericsForwards().Match(line, startMatchIndex, searchLength);
-            if(match.Success)
+            foreach (var match in NumbersGreedy().Matches(line).Cast<Match>())
             {
-                var partNumber = ExtractAdjacentPartNumber(line, match.Index, match.Value);
-                parts.Add(long.Parse(partNumber));
+                foreach (int symbolIndex in symbolIndices)
+                {
+                    //start1 <= end2 && end1 >= start2
+                    var searchStart = symbolIndex - 1;
+                    var SearchEnd = symbolIndex + 1;
+                    var numberStart = match.Index;
+                    var numberEnd = match.Index + match.Length - 1;
+
+                    if (searchStart <= numberEnd && SearchEnd >= numberStart)
+                    {
+                        partNumbers.Add(long.Parse(match.Value));
+                    }
+                }
             }
         }
 
-        return parts;
-    }
-
-    public string ExtractAdjacentPartNumber(string partNumberLine, int positionMatch, string initialAdjacentCharacter)
-    {
-        //OK we have a match now we have to read ahead and behind of the match position to get the whole string
-        //This is likely horribly inefficient
-        int startPos = positionMatch;
-        var numberString = initialAdjacentCharacter;
-
-        //read until we hit a non numeric
-        for(int i = startPos; i > 0; i--)
-        {
-            var substr = partNumberLine.Substring(i - 1, 1);
-            if(!NumericsForwards().Match(substr).Success)
-            {
-                break;
-            }
-            numberString = substr + numberString;
-        }
-
-        for(int i = startPos; i < partNumberLine.Length - 1; i++)
-        {
-            var substr = partNumberLine.Substring(i + 1, 1);
-            if(!NumericsForwards().Match(substr).Success)
-            {
-                break;
-            }
-            numberString += substr;
-        }
-
-        return numberString;
+        return partNumbers;
     }
 
     [GeneratedRegex("([^0-9\\.])")]
-    private static partial Regex SymbolNoNumbersNoDots();
+    public static partial Regex SymbolNoNumbersNoDots();
 
-    [GeneratedRegex("([0-9])")]
-    private static partial Regex NumericsForwards();
+    [GeneratedRegex("\\d+")]
+    private static partial Regex NumbersGreedy();
 }
 
