e782e167529a354ee6dd46ef9d198766931e90ac
Will Sneddon <will@sneddon.dev>
2023-12-09 20:59:25 -0500

    Initial Implementation with tests

---
+++ 04/Day4.Tests/ScratchCardProcessorTests.cs
@@ -0,0 +1,40 @@
+namespace Day4.Tests;
+
+public class ScratchCardProcessorTests
+{
+    [Theory]
+    [InlineData(@"testinput.txt", 6)]
+    public void BuildCardsFromFileShouldPass(string filename, int expectedCardCount)
+    {
+        var processor = new ScratchCardProcessor(filename);
+        processor.BuildCards();
+
+        Assert.Equal(expectedCardCount, processor.ScratchCards.Count());
+    }
+
+    [Theory]
+    [InlineData("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", 1, 8)]
+    [InlineData("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", 2, 2)]
+    [InlineData("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", 3, 2)]
+    [InlineData("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", 4, 1)]
+    [InlineData("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", 5, 0)]
+    [InlineData("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", 6, 0)]
+    public void BuildCardFromLineShouldPass(string gameLine, int expectedCardNo, int expectedScore)
+    {
+        var processor = new ScratchCardProcessor();
+        var card = processor.BuildCard(gameLine);
+
+        Assert.Equal(expectedCardNo, card.CardNo);
+        Assert.Equal(expectedScore, card.Score);
+    }
+
+    [Theory]
+    [InlineData(@"testinput.txt", 13)]
+    public void BuildCardTotalScoreFromFileShouldPass(string filename, int expectedScore)
+    {
+        var processor = new ScratchCardProcessor(filename);
+        var score = processor.GetTotalScore();
+
+        Assert.Equal(expectedScore, score);
+    }
+}
---
+++ 04/Day4/ScratchCardProcessor.cs
@@ -0,0 +1,97 @@
+
+using System.Text.RegularExpressions;
+
+public partial class ScratchCardProcessor
+{
+    private string InputFileName = string.Empty;
+
+    public IList<ScratchCard> ScratchCards { get; init; } = new List<ScratchCard>();
+
+    private const string CardSeparator = "|";
+
+    public ScratchCardProcessor(string fileName)
+    {
+        InputFileName = fileName;
+    }
+
+    public ScratchCardProcessor()
+    {
+
+    }
+
+    public int GetTotalScore()
+    {
+        BuildCards();
+        return ScratchCards.Sum(c => c.Score);
+    }
+
+    public void BuildCards()
+    {
+        ScratchCards.Clear();
+
+        foreach (var line in File.ReadLines(InputFileName))
+        {
+            ScratchCards.Add(BuildCard(line));
+        }
+    }
+
+    public ScratchCard BuildCard(string gameLine)
+    {
+        var cardMatcher = CardMatchRegex().Match(gameLine);
+        var cardNo = int.Parse(cardMatcher.Groups[1].Value);
+
+        gameLine = gameLine.Replace(cardMatcher.Groups[1].Value, string.Empty);
+
+        var numberMatcher = GameNumberRegex();
+        var splitIndex = gameLine.IndexOf(CardSeparator);
+
+        var card = new ScratchCard(cardNo);
+
+        foreach(var numberMatch in numberMatcher.Matches(gameLine).Cast<Match>())
+        {
+            int value = int.Parse(numberMatch.Value);
+            if (numberMatch.Index < splitIndex)
+            {
+                card.WinningNumbers.Add(value);
+            }
+            else
+            {
+                card.GameNumbers.Add(value);
+            }
+        }
+
+        return card;
+    }
+
+    [GeneratedRegex("Card ([0-9+]):")]
+    private static partial Regex CardMatchRegex();
+    [GeneratedRegex("([0-9]+[^:]?)")]
+    private static partial Regex GameNumberRegex();
+}
+
+public class ScratchCard
+{
+    public int CardNo { get; init; }
+
+    public IList<int> WinningNumbers { get; init; } = new List<int>();
+    public IList<int> GameNumbers { get; init; } = new List<int>();
+
+    public ScratchCard(int cardNo)
+    {
+        CardNo = cardNo;
+    }
+
+    public int Score 
+    {
+        get 
+        {
+            var intersections = WinningNumbers.Intersect(GameNumbers).Count();
+            if(intersections == 0)
+            {
+                return 0;
+            }
+
+            return (int)Math.Pow(2, intersections - 1);
+        }
+    }
+}
